35d1baebe4d047246d6d5d184b0a8849a892a9c3
sinacadonau <sinacadonau@gmail.com>
2016-11-18 08:50:37 +0100

    helloooo

--- html/fragebogen.php
+++ html/fragebogen.php
@@ -65,7 +65,6 @@
 <!-- Bootstrap Timeline -->
 <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,600,700' rel='stylesheet' type='text/css'>
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-<button id="toggleButton">Toggle</button>
 
 <ul class="timeline" id="timeline">
 
