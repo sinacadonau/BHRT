3c7d3af719edf5b28ac9010058331fc96d947f2b
Simone Schregenberger <simi.schregi@hotmail.com>
2016-11-27 16:53:22 +0100

    Profilbearbeitung

--- system/data.php
+++ system/data.php
@@ -45,4 +45,45 @@ function get_result($sql)
 	  		}
 
 
+		/* *********************************************************
+			/* Einstellungen
+			/* ****************************************************** */
+
+			function get_user($user_id){
+			    $sql = "SELECT * FROM user WHERE u_id = $user_id;";
+					return get_result($sql);
+				}
+
+				function update_user($user_id, $name, $email, $password, $confirm_password){
+			   	$sql_ok = false;
+			   	$sql = "UPDATE user SET ";
+					if($name != ""){
+						$sql .= "name = '$name', ";
+					 $sql_ok = true;
+					}
+					if($email != ""){
+			   		$sql .= "email = '$email', ";
+			   		$sql_ok = true;
+			     }
+			     if($password != "" && $password == $confirm_password) {
+			       $sql .= "password = '$password', ";
+			   		$sql_ok = true;
+			     }
+
+			     // Das Komma an der vorletzten Position des $sql-Strings durch ein Leerzeichen ersetzen
+			     $sql = substr_replace($sql, ' ', -2, 1);
+
+			     // $sql-String vervollständigen
+			     $sql .= " WHERE u_id = $user_id ;";
+
+			   	if($sql_ok){
+			   	  return get_result($sql);
+			   	}else{
+			   		return false;
+			   	}
+			   }
+
+
+
+
 ?>
